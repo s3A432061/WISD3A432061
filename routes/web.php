ddca7a9459a4a308a9a93387a9bb923ea50c8d1e
CZY <roeyang1410@gmail.com>
2017-10-05 19:27:23 +0800

    任務

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,51 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
+
+
+    /**
+     * 顯示所有任務
+     */
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+    /**
+     * 增加新的任務
+     */
+Route::post('/task', function (Request $request) {
+    // 驗證輸入
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // 建立該任務...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+
+    //新增任務存入DB的程式碼 (see next page)
+});
+
+
+/**
+     * 刪除任務
+     */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
 });
